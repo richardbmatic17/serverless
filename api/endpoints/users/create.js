9d7c9f9c43bc1557e2baab95818606fc69cbe57b
Richard Matic <rmatic@stratpoint.com>
2019-10-31 00:13:20 +0800

    add try catch

--- api/endpoints/users/create.js
+++ api/endpoints/users/create.js
@@ -2,13 +2,22 @@
 const db = require('../../database/models');
 
 module.exports.handler = async (event) => {
-  const params = JSON.parse(event.body);
-  const data = await db.User.create(params);
-  const response = {
-    status: data ? 'SUCCESS' : 'FAILED',
-    message: data ? 'Successfully fetched details' : 'Failed fetched details',
-    data,
-  };
-
-  return response;
+  try {
+    const params = JSON.parse(event.body);
+    const data = await db.User.create(params);
+    const response = {
+      status: data ? 'SUCCESS' : 'FAILED',
+      message: data ? 'Successfully fetched details' : 'Failed fetched details',
+      data,
+    };
+    return response;
+  } catch (error) {
+    console.log({error});
+    const response = {
+      status: 'ERROR',
+      message: 'Error creating user',
+      data: error,
+    };
+    return response;
+  }
 };
