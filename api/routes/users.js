cc42fd2f192f4ee9c698c70fed9a50fcdd0d9767
Richard Matic <rmatic@stratpoint.com>
2019-10-30 20:13:42 +0800

    add async await in user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,13 +5,13 @@ const {
 } = require('../endpoints/users');
 require('dotenv').config();
 
-module.exports.handler = (event, context, callback) => {
+module.exports.handler = async (event, context, callback) => {
   const funcName = event.pathParameters && event.pathParameters.funcName.toUpperCase();
   console.log(JSON.stringify({funcName}));
 
   switch (funcName) {
     case 'DETAILS':
-      const results = details(event);
+      const results = await details(event);
       callback(null, response(200, results));
       return;
     default: {
