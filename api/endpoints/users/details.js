81208b2a0d8362a180fba3d85bdc636db79e3ad0
Richard Matic <rmatic@stratpoint.com>
2019-11-01 13:56:53 +0800

    add semi colon

--- api/endpoints/users/details.js
+++ api/endpoints/users/details.js
@@ -4,9 +4,9 @@ const db = require('../../database/models');
 module.exports.handler = async (event) => {
   try {
     const params = event.queryStringParameters;
-    const filters = {where: {}}
+    const filters = {where: {}};
     if(params.id) {
-      filters['where']['id'] = params.id
+      filters['where']['id'] = params.id;
     }
     const data = await db.Users.findAll(filters);
     
